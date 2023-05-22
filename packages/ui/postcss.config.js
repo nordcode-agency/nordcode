ea4349c1a181c9f8ea2728ba2e3eb14b4652f5eb
Lukas Koeller <acc.koeller@protonmail.com>
2023-05-22 23:03:15 +0200

    refactor(ui): use @nordcode/postcss-config

--- packages/ui/postcss.config.js
+++ packages/ui/postcss.config.js
@@ -1,28 +1,7 @@
-const postcssPresetEnv = require("postcss-preset-env");
-const postcssImport = require("postcss-import");
-const combineSelectors = require("postcss-combine-duplicated-selectors");
-
-// copied from open props
-// https://github.com/argyleink/open-props/blob/main/postcss.config.cjs
+const config = require('@nordcode/postcss-config');
 
 module.exports = {
   plugins: [
-    postcssImport(),
-    postcssPresetEnv({
-      stage: 0,
-      autoprefixer: false,
-      features: {
-        "logical-properties-and-values": false,
-        "prefers-color-scheme-query": false,
-        "gap-properties": false,
-        "custom-properties": false,
-        "place-properties": false,
-        "not-pseudo-class": false,
-        "focus-visible-pseudo-class": false,
-        "focus-within-pseudo-class": false,
-        "color-functional-notation": false,
-      },
-    }),
-    combineSelectors(),
+    ...config.plugins,
   ],
 };
