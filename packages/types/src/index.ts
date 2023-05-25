4fe64a36147fc14f7ef72fdd2ae4b6aec0ab258a
Lukas Koeller <acc.koeller@protonmail.com>
2023-05-25 23:28:13 +0200

    feat(types): new types package!

---
+++ packages/types/src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Sizes used e.g. in components for `size` prop.
+ * 
+ * @example
+ * ```svelte
+ * <Card size={Size.Sm}></Card>
+ * ```
+ */
+export enum Size {
+  Xs = 'xs',
+  Sm = 'sm', 
+  Md = 'md',
+  Lg = 'lg',
+  Xl = 'xl',
+  Xxl = 'xxl',
+}
