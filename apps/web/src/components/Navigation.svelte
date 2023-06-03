097257a3b009e8ca1dac1993547d184762b49551
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-03 23:27:44 +0200

    feat(web): add Navigation

---
+++ apps/web/src/components/Navigation.svelte
@@ -0,0 +1,27 @@
+<nav>
+  <ul>
+    <li><a href="#">Work</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Let's start</a></li>
+  </ul>
+</nav>
+
+<style lang="postcss">
+  nav {
+    padding: var(--spacing-base);
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  a {
+    font-size: var(--font-size-largest);
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  li {
+    text-align: end;
+  }
+</style>
