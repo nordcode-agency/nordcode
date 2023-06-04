6d4516bea0229c4b29e1a7a845bc1bfd462476e2
JSHSJ <kontakt@joshuastuebner.com>
2023-06-04 16:19:16 +0200

    feat(ui): add dialog

---
+++ packages/ui/src/modules/dialogs/svelte/dialog.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+
+</script>
+
+
+<dialog data-id="dialog-1" data-level="1">
+      <div class="dialog-container">
+        <div class="dialog-header">
+          <h2 class="dialog-title">I'm a dialog (1)</h2>
+          <button data-closes-dialog="dialog-1">X</button>
+        </div>
+        <div class="dialog-content">
+          <button data-opens-dialog="dialog-2">Show me the next dialog</button>
+        </div>
+      </div>
+    </dialog>
---
+++ packages/ui/src/modules/dialogs/ts/dialogs.ts
@@ -0,0 +1,49 @@
+const openButtons = document.querySelectorAll<HTMLButtonElement>(
+  "button[data-opens-dialog]"
+);
+const closeButtons = document.querySelectorAll<HTMLButtonElement>(
+  "button[data-closes-dialog]"
+);
+const openDialogs: HTMLDialogElement[] = [];
+
+openButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const dialog = document.querySelector<HTMLDialogElement>(
+      `[data-id="${button.dataset.opensDialog}"]`
+    );
+    dialog.showModal();
+    openDialogs.forEach((d, idx) => {
+      d.style.setProperty(
+        "--dialog-inative-offset",
+        `${-1 * (openDialogs.length + 1 - idx)}dvh`
+      );
+      d.classList.add("-inactive");
+    });
+    openDialogs.push(dialog);
+  });
+});
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    closeCurrentDialog();
+  });
+});
+
+const closeCurrentDialog = () => {
+  const currentDialog = openDialogs.pop();
+  if (!currentDialog) {
+    return;
+  }
+
+  currentDialog.addEventListener(
+    "animationend",
+    () => {
+      currentDialog.classList.remove("-closing");
+      currentDialog.close();
+      openDialogs[openDialogs.length - 1].classList.remove("-inactive");
+    },
+    { once: true }
+  );
+
+  currentDialog.classList.add("-closing");
+};
