c005d8d944effdc4ce1ee4e56ae7e21bdbac4e08
Lukas Koeller <acc.koeller@protonmail.com>
2023-06-07 21:51:46 +0200

    feat(web): add Work component

---
+++ apps/web/src/components/Work.svelte
@@ -0,0 +1,20 @@
+<section class="nc-ram-grid">
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+  <div class="nc-card">a</div>
+</section>
+
+<style>
+  .nc-ram-grid {
+    --fd-ram-grid-min-width: 300px;
+
+    padding: var(--spacing-base);
+  }
+</style>
