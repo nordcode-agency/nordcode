728419315191476ceaf57c3acf809901ee6ea11a
JSHSJ <kontakt@joshuastuebner.com>
2023-06-19 21:48:02 +0200

    feat(ui): add shadow color

--- packages/ui/src/modules/forms/svelte/InputFields/Input.svelte
+++ packages/ui/src/modules/forms/svelte/InputFields/Input.svelte
@@ -3,6 +3,7 @@
     import GenericInput from "./GenericInput.svelte";
     import DateInput from "./DateInput.svelte";
     import CheckboxInput from "./CheckboxInput.svelte";
+    import RangeInput from "./RangeInput.svelte";
 
     export let label: string;
     export let name: string;
@@ -17,14 +18,17 @@
 </script>
 
 {#if type === "checkbox"}
-    <CheckboxInput {id} {name} bind:value={value} {label} {autocomplete} {optional} {errors} {hint} />
+    <CheckboxInput {id} {name} bind:value={value} {label} {autocomplete} {optional} {errors} {hint}/>
+
 {:else}
-<InputWrapper {id} {label} {optional} {errors} {hint}>
-    {#if type === 'date'}
-    <DateInput {id} {name} {optional} {autocomplete} bind:value={value}/>
-    {:else}
-        <GenericInput {id} {name} {optional} {autocomplete} {type} bind:value={value} {...$$restProps} />
-    {/if}
-    <slot></slot>
-</InputWrapper>
+    <InputWrapper {id} {label} {optional} {errors} {hint}>
+        {#if type === 'date'}
+            <DateInput {id} {name} {optional} {autocomplete} bind:value={value}/>
+        {:else if type === "range"}
+            <RangeInput {id} {name} {optional} {autocomplete} {type} bind:value={value} {...$$restProps}/>
+        {:else}
+            <GenericInput {id} {name} {optional} {autocomplete} {type} bind:value={value} {...$$restProps}/>
+        {/if}
+        <slot></slot>
+    </InputWrapper>
 {/if}
---
+++ packages/ui/src/modules/forms/svelte/InputFields/RangeInput.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let name: string;
+    export let id: string;
+    export let errors: string[] = [];
+    export let hint: string = '';
+    export let autocomplete: string = 'off';
+    export let type: string = 'text';
+    export let optional: boolean = false;
+
+    export let value;
+
+    const handleInput = (event: InputEvent) => {
+        // in here, you can switch on type and implement
+        // whatever behaviour you need
+        const target = event.target as HTMLInputElement
+
+        if (type.match(/^(number|range)$/)) {
+            value = +target.value
+        }
+        value = target.value
+    }
+
+</script>
+
+<div class="stack -full-width -layout-none">
+<input class="nc-input"
+       id={id}
+       name={name}
+       aria-required={!optional}
+       autocomplete={autocomplete}
+       type={type}
+       value={value}
+       on:input={handleInput}
+       {...$$restProps}
+>
+    <small class="value">{value}</small>
+</div>
+
+<style>
+    .value {
+        color: var(--color-text-subtle);
+        margin-inline-start: auto;
+    }
+</style>
+
