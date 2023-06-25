2a2245417712138ef63cf8909d3aef7829eaca9b
JSHSJ <kontakt@joshuastuebner.com>
2023-06-25 19:01:39 +0200

    feat(ui): add lightning css

---
+++ packages/ui/transform.js
@@ -0,0 +1,31 @@
+import { browserslistToTargets, bundle } from "lightningcss";
+import browserslist from "browserslist";
+import fs from "fs";
+
+let targets = browserslistToTargets(browserslist(">= 0.25%"));
+
+let { code } = bundle({
+  // ...
+  targets,
+  drafts: {
+    nesting: true,
+    customMedia: true,
+  },
+  filename: "src/styles/bundle.css",
+  // minify: true,
+});
+
+fs.writeFileSync("out/bundle.css", code);
+
+let { code: codeComplete } = bundle({
+  // ...
+  targets,
+  drafts: {
+    nesting: true,
+    customMedia: true,
+  },
+  filename: "src/styles/complete.css",
+  // minify: true,
+});
+
+fs.writeFileSync("out/complete.css", codeComplete);
