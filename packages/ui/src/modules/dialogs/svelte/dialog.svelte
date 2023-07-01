1d841bea7cc5d1811b0184e473e1f799c737f7c1
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-01 23:38:40 +0200

    refactor(ui): dialog

--- packages/ui/src/modules/dialogs/svelte/dialog.svelte
+++ packages/ui/src/modules/dialogs/svelte/dialog.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
   export let heading: string = '';
+  export const id: string = '';
 </script>
 
 
 <dialog
   class="nc-dialog"
-  data-id="dialog-1"
+  {id}
   data-level="1"
   inert
-  loading
 >
-  <div class="dialog-container">
+  <form method="dialog" class="dialog-container">
     <div class="dialog-header">
       <h2 class="dialog-title">{heading}</h2>
-      <button data-closes-dialog="dialog-1">X</button>
+      <button data-closes-dialog>X</button>
     </div>
     <div class="dialog-content">
       <slot />
       <button data-opens-dialog="dialog-2">Show me the next dialog</button>
     </div>
-  </div>
+  </form>
 </dialog>
