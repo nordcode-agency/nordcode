55400344480801c20eb5413e81e5ba4c48d8e563
JSHSJ <kontakt@joshuastuebner.com>
2023-07-07 15:08:32 +0200

    feat: refactor web to svelte-kit + lightning

---
+++ apps/web/src/routes/+layout.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+
+  import Footer from '$lib/modules/common/components/Footer.svelte';
+
+  import {onMount} from "svelte";
+  import {page} from "$app/stores";
+
+  onMount(async () => {
+    await import ("@nordcode/ui/src/modules/dialogs/ts/dialogs.ts")
+  })
+</script>
+
+
+<svelte:head>
+  <title>{$page.data.title}</title>
+  <meta name="description" content={$page.data.description}/>
+</svelte:head>
+
+<main class="nc-stack">
+  <slot/>
+</main>
+<Footer/>
+
+<style>
+  @import "@nordcode/ui/complete";
+  @import "@nordcode/ui/fonts";
+</style>
---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Header from "$lib/modules/common/components/Header.svelte";
+  import Hero from "$lib/modules/common/components/Hero.svelte";
+  import Navigation from "$lib/modules/common/components/Navigation.svelte";
+  import Work from "$lib/modules/common/components/Work.svelte";
+</script>
+
+<svelte:head>
+
+</svelte:head>
+
+<div class="wrapper">
+		<Header />
+		<Hero />
+		<Navigation />
+	</div>
+	<Work />
+
+<style lang="postcss">
+	.wrapper {
+		display: grid;
+    grid-template-rows: auto 1fr auto;
+		block-size: 100svh;
+		padding-inline: var(--spacing-base);
+		padding-block-end: var(--spacing-base);
+
+		/*@media (--md-n-above) {*/
+		/*	padding-inline: var(--spacing-farthest);*/
+		/*	padding-block-end: var(--spacing-farthest);*/
+		/*}*/
+	}
+</style>
