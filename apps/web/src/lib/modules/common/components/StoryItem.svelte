9036675ec008cdbf20f311de6041fd88c0639769
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-09 00:13:01 +0200

    feat(web): add storyitem component

---
+++ apps/web/src/lib/modules/common/components/StoryItem.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  export let caption: string;
+</script>
+
+<figure>
+  <slot />
+  {#if caption}
+    <figcaption>
+      {caption}
+    </figcaption>
+  {/if}
+</figure>
+
+<style lang="postcss">
+  figure {
+    inline-size: 100%;
+    block-size: 100%;
+    display: grid;
+    grid-template: [fig-start] 1fr [cap-start] auto [cap-end fig-end] / [cap-start fig-start] 1fr [cap-start fig-end];
+    
+    & :global(> :not(figcaption)) {
+      inline-size: 100%;
+      block-size: 100%;
+      object-fit: cover;
+      grid-area: fig;
+    }
+  }
+
+  figcaption {
+    grid-area: cap;
+    inline-size: 100%;
+    padding-inline: var(--spacing-base);
+    padding-block: var(--spacing-far);
+    background: linear-gradient(transparent, rgba(0,0,0,0.6));
+    font-size: var(--font-size-large);
+  }
+</style>
