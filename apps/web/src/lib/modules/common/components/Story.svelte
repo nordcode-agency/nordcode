59898f8c49d9a6728ef550a220a40ff0fad7a599
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-09 00:12:51 +0200

    feat(web): add story component

---
+++ apps/web/src/lib/modules/common/components/Story.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let id: string;
+</script>
+
+<dialog {id}>
+  <ul class="nc-gallery">
+    <slot />
+  </ul>
+</dialog>
+
+<style lang="postcss">
+  dialog {
+    padding: 0;
+    aspect-ratio: 9/16;
+    inline-size: min(48ch, 100%);
+    border-radius: var(--border-radius-large);
+  }
+
+  .nc-gallery {
+    inline-size: 100%;
+    block-size: 100%;
+  }
+</style>
