b89b6dc02c98983d8cd1e6df8d4e321dd40d0cb0
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-10 17:14:29 +0200

    style(web): add to StoryItem

--- apps/web/src/lib/modules/common/components/StoryItem.svelte
+++ apps/web/src/lib/modules/common/components/StoryItem.svelte
@@ -3,26 +3,54 @@
 </script>
 
 <figure>
-  <slot />
+  <div class="figure">
+    <slot />
+  </div>
   {#if caption}
+    <div class="gradient"></div>
     <figcaption>
       {caption}
     </figcaption>
   {/if}
+  {#if $$slots.cta}
+    <div class="cta">
+      <slot name="cta" />
+    </div>
+  {/if}
 </figure>
 
 <style lang="postcss">
+  @import "@nordcode/ui/media";
+
   figure {
     inline-size: 100%;
     block-size: 100%;
     display: grid;
-    grid-template: [fig-start] 1fr [cap-start] auto [cap-end fig-end] / [cap-start fig-start] 1fr [cap-start fig-end];
-    
-    & :global(> :not(figcaption)) {
+    grid-template:
+      [fig-start actions-start]
+      1fr
+      [cap-start cta-start actions-end]
+      auto
+      [cap-end fig-end cta-end]
+      / [cap-start fig-start] 1fr [cap-end cta-start actions-start] auto [fig-end cta-end actions-end];
+  }
+
+  .gradient {
+    grid-area: cap-start / cap-start / cap-end / cta-end;
+    background: linear-gradient(transparent, rgba(0,0,0,0.6));
+    inline-size: 100%;
+    block-size: 100%;
+  }
+
+  .figure {
+    grid-area: fig;
+    inline-size: 100%;
+    block-size: 100%;
+
+    & :global(> *) {
       inline-size: 100%;
       block-size: 100%;
       object-fit: cover;
-      grid-area: fig;
     }
   }
 
@@ -31,7 +59,23 @@
     inline-size: 100%;
     padding-inline: var(--spacing-base);
     padding-block: var(--spacing-far);
-    background: linear-gradient(transparent, rgba(0,0,0,0.6));
     font-size: var(--font-size-large);
+    color: var(--color-text-dark-base);
+  }
+
+  .cta {
+    grid-area: cta;
+    display: grid;
+    place-items: center;
+    block-size: 100%;
+    padding-block: var(--spacing-far);
+    padding-inline-end: var(--spacing-base);
+  }
+
+  .actions {
+    display: flex;
+    flex-direction: column-reverse;
+    gap: var(--spacing-base);
+    padding: var(--spacing-base);
   }
 </style>
