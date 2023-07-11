417e5a583b179ac8314621d58e0da413c36c0b42
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-11 22:10:05 +0200

    feat(web): add indicator for Story

--- apps/web/src/lib/modules/common/components/Hero.svelte
+++ apps/web/src/lib/modules/common/components/Hero.svelte
@@ -15,16 +15,16 @@
         Preview
       </button>
       <Story id="preview">
-        <StoryItem caption="My individual caption that tells a short story.">
+        <StoryItem caption="My individual caption that tells a short story." id="story-1">
           <img src="https://images.unsplash.com/photo-1688367785310-c8c013548288?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3135&q=80" alt="storyitem 1" srcset="">
           <button type="button" class="nc-button" slot="cta" aria-label="Mehr Erfahren">
             <ArrowRightSolid />
           </button>
         </StoryItem>
-        <StoryItem caption="Here is a good example on how your web app could look like">
+        <StoryItem caption="Here is a good example on how your web app could look like" id="story-2">
           <img src="https://images.unsplash.com/photo-1687369500069-a0872397b3ca?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3125&q=80" alt="storyitem 2" srcset="">
         </StoryItem>
-        <StoryItem caption="Sustainability in and out">
+        <StoryItem caption="Sustainability in and out" id="story-3">
           <img src="https://images.unsplash.com/photo-1558297010-eb4f2f472abf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2246&q=80" alt="storyitem 3" srcset="">
         </StoryItem>
       </Story>
--- apps/web/src/lib/modules/common/components/Story.svelte
+++ apps/web/src/lib/modules/common/components/Story.svelte
@@ -1,15 +1,43 @@
 <script lang="ts">
   export let id: string;
+
+  let ulElement: HTMLUListElement;
+  $: items = Array.from(ulElement?.children ?? []).map((item) => {
+    return {
+      id: item.id,
+      text: (item as HTMLLIElement).innerText,
+    };
+  });
+
+  const handleClick = (e: MouseEvent, id: string) => {
+    const el = document.getElementById(id);
+    el?.scrollIntoView({ behavior: 'smooth' });
+  }
 </script>
 
 <dialog {id}>
-  <ul class="nc-gallery">
+  <ul class="nc-gallery" bind:this={ulElement}>
     <slot />
   </ul>
+  <div class="controls">
+    <div class="indicator">
+      {#each items as {id, text} (id)}
+        <button
+          class="nc-button -stealth"
+          on:click={(e) => handleClick(e, id)}
+          aria-label={text}
+        >
+          <div class="indicator-item"></div>
+        </button>
+      {/each}
+    </div>
+  </div>
 </dialog>
 
 <style lang="postcss">
   dialog {
+    display: grid;
+    grid-template: [gallery-start controls-start] auto [controls-end] 1fr [gallery-end] / [controls-start gallery-start] 1fr [controls-end gallery-end];
     padding: 0;
     aspect-ratio: 9/16;
     inline-size: min(48ch, 100%);
@@ -17,10 +45,47 @@
   }
 
   .nc-gallery {
+    grid-area: gallery;
     --gallery-padding-inline: 0;
     --gallery-scroll-snap-stop: always;
 
     inline-size: 100%;
     block-size: 100%;
+  }
+
+  .controls {
+    grid-area: controls;
+    inline-size: 100%;
+    block-size: auto;
+    padding-inline: var(--spacing-base);
+    padding-block: 0;
+    background: linear-gradient(rgba(0,0,0,0.6), transparent);
+  }
+
+  .indicator {
+    display: grid;
+    grid-auto-flow: column;
+    gap: var(--spacing-near);
+  }
+
+  .nc-button {
+    --button-border-radius: 0;
+
+    padding-block: var(--spacing-base);
+    padding-inline: 0;
+    background-color: transparent;
+    aspect-ratio: auto;
+
+    &:hover .indicator-item {
+      background-color: rgba(255,255,255,0.8);
+    }
+  }
+
+  .indicator-item {
+    block-size: 4px;
+    inline-size: 100%;
+    background-color: rgba(255,255,255,0.5);
+    border-radius: var(--border-radius-round);
+    transition: background-color var(--transition-duration-base) var(--ease-2);
   }
 </style>
