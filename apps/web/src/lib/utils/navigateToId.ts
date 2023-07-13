c84ae3487f75d03f79bafb69eaa5d930c4317187
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-13 23:29:06 +0200

    feat(web): add navigateToId util

---
+++ apps/web/src/lib/utils/navigateToId.ts
@@ -0,0 +1,13 @@
+/**
+ * Either navigates or scrolls element into view with a given `id`.
+ * This can be helpful to scroll horizontal galleries by clicking.
+ * @param {string} id The `id` attribute value of the HTML element to be navigated to.
+ */
+export const navigateToId = (id: string) => {
+  if (navigation.navigate) {
+    navigation.navigate(`#${id}`, { history: 'replace' });
+  } else {
+    const el = document.getElementById(id);
+    el?.scrollIntoView({ behavior: 'smooth' });
+  }
+}
