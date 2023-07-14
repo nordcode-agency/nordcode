6a021ec3b68d43457ae17a94776ffe77c4dc5405
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-14 08:16:09 +0200

    fix(web): correctly check for navigation support

--- apps/web/src/lib/utils/navigateToId.ts
+++ apps/web/src/lib/utils/navigateToId.ts
@@ -3,9 +3,9 @@
  * This can be helpful to scroll horizontal galleries by clicking.
  * @param {string} id The `id` attribute value of the HTML element to be navigated to.
  */
-export const navigateToId = (id: string) => {
-  if (navigation.navigate) {
-    navigation.navigate(`#${id}`, { history: 'replace' });
+export const navigateToId = (id: string) => { 
+  if (document?.navigation?.navigate) {
+    document.navigation.navigate(`#${id}`, { history: 'replace' });
   } else {
     const el = document.getElementById(id);
     el?.scrollIntoView({ behavior: 'smooth' });
