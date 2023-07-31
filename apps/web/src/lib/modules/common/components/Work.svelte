95344854938ce31a06d4a59503a13fbfa49ae946
Lukas Koeller <acc.koeller@protonmail.com>
2023-07-31 19:35:14 +0200

    chore(web): work on Work and WorkCard

--- apps/web/src/lib/modules/common/components/Work.svelte
+++ apps/web/src/lib/modules/common/components/Work.svelte
@@ -2,8 +2,8 @@
 	import WorkCard from "./WorkCard.svelte";
 
 </script>
+
 <section class="nc-ram-grid">
-  
   <WorkCard />
   <WorkCard />
   <WorkCard />
@@ -12,8 +12,31 @@
 </section>
 
 <style>
+  section {
+    position: relative;
+    background-image: url($lib/assets/polestar.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: color-burn;
+    /* @todo NEXT add a patterns with clip blend whatever and let polestar rotate slowly */
+  }
+
+  .bg-text {
+    position: absolute;
+    top: 0px;
+    color: var(--color-brand-primary-base);
+    font-weight: var(--font-weight-bold);
+    font-size: 50rem;
+    line-height: 20rem;
+    color: oklch(var(--color-brand-primary-base-lch) / 0.2);
+    /* background: linear-gradient(to left, var(--color-brand-secondary-strong), var(--color-brand-primary-strong)); */
+    /* -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent; */
+  }
   .nc-ram-grid {
     --fd-ram-grid-min-width: 300px;
+    --fd-ram-grid-gap: var(--spacing-far);
 
     padding: var(--spacing-base);
   }
--- apps/web/src/lib/modules/common/components/WorkCard.svelte
+++ apps/web/src/lib/modules/common/components/WorkCard.svelte
@@ -1,4 +1,14 @@
-<div class="nc-card">
+<div class="nc-card card">
   <span>Farmers Snack</span>
   <h2>A digital taste of your snack</h2>
 </div>
+
+<style lang="postcss">
+  .card {
+    background: linear-gradient(45deg, oklch(var(--color-border-base-lch) / 0.03), oklch(var(--color-surface-base-lch) / 0.8));
+    backdrop-filter: blur(var(--spacing-base));
+    box-shadow:
+      inset 0px 0px var(--spacing-base) calc(var(--spacing-near) * -1) oklch(var(--color-border-base-lch) / 0.2);
+    aspect-ratio: 4 / 5;
+  }
+</style>
