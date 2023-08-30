c6d4d7c40aec4b1d2a27d77b4f92edf2f4b7dea3
Lukas Koeller <acc.koeller@protonmail.com>
2023-08-30 10:06:22 +0200

    chore(web): try different layout

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import Hero from "$lib/modules/common/components/Hero.svelte";
   import Work from "$lib/modules/common/components/Work.svelte";
+	import WorkCard from "$lib/modules/common/components/WorkCard.svelte";
 </script>
 
 <svelte:head>
@@ -8,4 +9,38 @@
 </svelte:head>
 
 <Hero />
-<Work />
+<!-- <Work /> -->
+<div class="section row">
+  <WorkCard />
+  <WorkCard />
+</div>
+<div class="section row">
+  <div class="highlight">
+    <WorkCard />
+  </div>
+</div>
+
+<style lang="postcss">
+  @import "@nordcode/ui/media";
+  .row {
+    display: grid;
+    row-gap: var(--spacing-farthest);
+    column-gap: var(--spacing-far);
+
+    @media (--md-n-above) {
+      grid-template-columns: repeat(2, 1fr);
+
+      & .highlight {
+        grid-column: span 2;
+      }
+    }
+
+    @media (--lg-n-above) {
+      grid-template-columns: repeat(3, 1fr);
+
+      & .highlight {
+        grid-column: 2 / 4;
+      }
+    }
+  }
+</style>
