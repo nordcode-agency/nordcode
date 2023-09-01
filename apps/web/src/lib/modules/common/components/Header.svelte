e550a1a86b2cd43e3b35e91d265725c543853c4f
Lukas Koeller <acc.koeller@protonmail.com>
2023-09-01 23:01:31 +0200

    chore(web): adjust Header

--- apps/web/src/lib/modules/common/components/Header.svelte
+++ apps/web/src/lib/modules/common/components/Header.svelte
@@ -29,7 +29,8 @@
 
 	nav {
 		pointer-events: auto;
-		inline-size: min(100%, 60ch);
+		/* inline-size: min(100%, 60ch); */
+		inline-size: 100%;
 		display: grid;
 		grid:
 			"home home work" auto
@@ -37,8 +38,8 @@
 			/ 1fr 1fr auto;
 		align-items: center;
 		border-radius: var(--border-radius-large);
-		background: oklch(var(--color-surface-subtle-lch) / 0.8);
-		backdrop-filter: blur(16px);
+		/* background: oklch(var(--color-surface-subtle-lch) / 0.8); */
+		/* backdrop-filter: blur(16px); */
 		overflow: hidden;
 
 		@media (--sm-n-above) {
@@ -68,7 +69,7 @@
 		inline-size: 100%;
     font-size: calc(var(--font-size-base) * 1.5);
     text-decoration: none;
-    color: var(--color-brand-primary-emphasis);
+    color: var(--color-brand-primary-base);
     padding-inline: var(--spacing-base);
     /* padding-block: var(--spacing-nearest); */
 		text-align: center;
