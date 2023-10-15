99ee41c7aa65b29d2c2a7c24a64de0af4106c492
Lukas Koeller <acc.koeller@protonmail.com>
2023-10-15 23:01:12 +0200

    chore(web): only apply view transition where click action

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,12 +3,21 @@
   import Footer from '$lib/modules/common/components/Footer.svelte';
   import { onNavigate } from '$app/navigation';
 
-  onNavigate((navigation) => {
+  onNavigate((navigation) => { 
       if (!document.startViewTransition) return;
+      const pathname = navigation?.to?.url.pathname;
+      const anchorEl = document.querySelector(`[href="${pathname}"]`);
+
+      /** @url https://developer.chrome.com/docs/web-platform/view-transitions/#transitioning-elements-dont-need-to-be-the-same-dom-element */
+      const figureEl = anchorEl?.querySelector('figure');
+      const h2El = anchorEl?.querySelector('h2');
+      if (figureEl) figureEl.style.viewTransitionName = 'work-figure';
+      if (h2El) h2El.style.viewTransitionName = 'work-headings';
 
       return new Promise((resolve) => {
           document.startViewTransition(async () => {
-              resolve();
+            resolve();
+            // if (anchorEl) anchorEl.removeAttribute('data-transition');
               await navigation.complete;
           });
       });
@@ -28,7 +37,6 @@
   <meta name="description" content={$page.data.description}/>
 </svelte:head>
 
-<Header />
 <main>
   <slot/>
 </main>
