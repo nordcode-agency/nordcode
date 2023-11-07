2662a823098065e56bea30781cb70583728d0892
Lukas Koeller <acc.koeller@protonmail.com>
2023-11-07 23:17:20 +0100

    chore(web): rename content to fileName

--- apps/web/src/lib/content/index.ts
+++ apps/web/src/lib/content/index.ts
@@ -1,16 +1,11 @@
-import type { MediaImage } from "$lib/types";
+import type { WorkProject } from "$lib/types";
 
-export const projects: {
-  heading: string;
-  subheading: string;
-  cover: MediaImage;
-  slug: string;
-}[] = [
+export const projects: WorkProject[] = [
   {
     heading: 'Telling a new story',
     subheading: 'Ricola',
     cover: {
-      src: 'https://images.unsplash.com/photo-1692912364084-97b9ae31a8e1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80',
+      fileName: 'ricola',
       alt: 'Farmers Snack'
     },
     slug: 'ricolax',
@@ -19,7 +14,7 @@ export const projects: {
     heading: 'A sky full of stars',
     subheading: 'Hotplay',
     cover: {
-      src: 'https://images.unsplash.com/photo-1692911070553-83862c0078d5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80',
+      fileName: 'new-york',
       alt: 'Farmers Snack'
     },
     slug: 'sky-full-of-stars',
@@ -28,7 +23,7 @@ export const projects: {
     heading: 'A digital taste of your snack',
     subheading: 'Farmers Snack',
     cover: {
-      src: 'https://images.unsplash.com/photo-1690738083729-f5d21709425c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3165&q=80',
+      fileName: 'mountains',
       alt: 'Farmers Snack'
     },
     slug: 'farmers-snack',
@@ -37,7 +32,7 @@ export const projects: {
     heading: 'Feeling home in Italy',
     subheading: 'Limuncetti',
     cover: {
-      src: 'https://images.unsplash.com/photo-1691866068948-fd8869ef4094?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3174&q=80',
+      fileName: 'italy',
       alt: 'Limuncetti Rentals'
     },
     slug: 'limuncetti',
@@ -46,7 +41,7 @@ export const projects: {
     heading: 'Reshaping transparency',
     subheading: 'Reshape',
     cover: {
-      src: 'https://images.unsplash.com/photo-1690342824253-4d9488961ded?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80',
+      fileName: 'ice',
       alt: 'Reshape New Web App'
     },
     slug: 'reshape',
@@ -55,7 +50,7 @@ export const projects: {
     heading: 'Travel the web',
     subheading: 'Traverly',
     cover: {
-      src: 'https://images.unsplash.com/photo-1682687219356-e820ca126c92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2970&q=80',
+      fileName: 'desert',
       alt: 'A new digital travel experience'
     },
     slug: 'traverly',
