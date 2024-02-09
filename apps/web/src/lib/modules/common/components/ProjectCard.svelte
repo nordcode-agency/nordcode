660d9ecbdd82b78ff64ac63e3db48f31eb900296
Lukas Koeller <acc.koeller@protonmail.com>
2024-02-09 23:24:15 +0100

    feat: add ProjectCard adding vt handling for FeatureCard
    vt: view transition

---
+++ apps/web/src/lib/modules/common/components/ProjectCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts" async>
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	import type { MediaImage } from '$lib/types/index';
+	import FeatureCard from './FeatureCard.svelte';
+
+	export let heading: string;
+	export let subheading: string | undefined = undefined;
+	export let cover: MediaImage;
+	export let slug: string;
+	export let name: string;
+
+	let vtName = '';
+	beforeNavigate((navigation) => {
+		const projectName = navigation?.to?.params?.project;
+		if (projectName === name) {
+			vtName = name;
+		}
+	});
+	afterNavigate(() => {
+		vtName = '';
+	});
+</script>
+
+<FeatureCard {heading} {subheading} {cover} {slug} id={name} />
