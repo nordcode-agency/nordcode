3ce0143ee8ec76fdafa4c6c2e9f337e0d22749ad
Lukas Koeller <acc.koeller@protonmail.com>
2024-02-10 23:26:36 +0100

    chore: progress on /start

--- apps/web/src/routes/start/+page.svelte
+++ apps/web/src/routes/start/+page.svelte
@@ -2,7 +2,6 @@
 	import Header from "$lib/modules/common/components/Header.svelte";
 	import type { PageData } from "../$types";
 
-
 	export let data: PageData;
 
   const { questions } = data;
@@ -14,24 +13,34 @@
 
 <div class="nc-stack fullscreen">
   <Header />
-  <section class="nc-stack nc-center">
-    <h1>Was ist dein Plan?</h1>
-    <h2>Wähle die Option, die am besten für dich passt.</h2>
-    <div class="nc-ram-grid grid">
+  <section class="nc-stack -farthest nc-center">
+    <div class="nc-stack">
+        <h1>Wähle die Option, die am besten für dich passt.</h1>
+    </div>
+    <div class="nc-ram-grid -base grid">
       {#each questions as question}
-        <div class="nc-card">
-          <h3>{question.heading}</h3>
-          <p>{question.desc}</p>
-        </div>
+        <a href={question.slug} class="nc-card card">
+            <div class="nc-stack">
+                <h3>{question.heading}</h3>
+                <p>{question.desc}</p>
+            </div>
+        </a>
       {/each}
     </div>
   </section>
 </div>
 
 <style lang="postcss">
-  .grid {
-    --nc-ram-grid-gap: var(--spacing-far);
-    --nc-ram-grid-min-width: 40ch;
+    h1 {
+        font-size: var(--font-size-display);
+    }
 
+  .grid {
+    --nc-ram-grid-gap: var(--spacing-base);
+    --nc-ram-grid-min-width: 40ch;
+  }
+
+  .button {
+    align-self: end;
   }
 </style>
