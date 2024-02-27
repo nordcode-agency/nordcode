da70e6bf4707600e79c63d5f76b24ae5ee73e8e3
Joshua Stübner <joshua.stuebner@orgavision.com>
2024-02-27 17:21:39 +0100

    feat(ui): use lightning css and lightdark to declare config colors

--- packages/ui/transform.js
+++ packages/ui/transform.js
@@ -1,44 +1,53 @@
-import { browserslistToTargets, bundle } from "lightningcss";
-import browserslist from "browserslist";
-import fs from "fs";
+import { browserslistToTargets, bundle } from 'lightningcss';
+import browserslist from 'browserslist';
+import fs from 'fs';
 
-let targets = browserslistToTargets(browserslist(">= 0.25%"));
+const targets = browserslistToTargets(browserslist('>= 0.25%'));
 
-let { code } = bundle({
-  // ...
-  targets,
-  drafts: {
-    nesting: true,
-    customMedia: true,
-  },
-  filename: "src/styles/bundle.css",
-  // minify: true,
+const { code: colors } = bundle({
+    // ...
+    targets,
+    drafts: {
+        customMedia: true,
+    },
+    filename: 'src/styles/theme/colors_raw.css',
+    // minify: true,
 });
 
-fs.writeFileSync("out/bundle.css", code);
+fs.writeFileSync('src/styles/theme/colors_processed.css', colors);
 
-let { code: codeComplete } = bundle({
-  // ...
-  targets,
-  drafts: {
-    nesting: true,
-    customMedia: true,
-  },
-  filename: "src/styles/complete.css",
-  // minify: true,
+const { code } = bundle({
+    // ...
+    targets,
+    drafts: {
+        customMedia: true,
+    },
+    filename: 'src/styles/bundle.css',
+    // minify: true,
 });
 
-fs.writeFileSync("out/complete.css", codeComplete);
+fs.writeFileSync('out/bundle.css', code);
 
-let { code: codeCompleteConfigless } = bundle({
-  // ...
-  targets,
-  drafts: {
-    nesting: true,
-    customMedia: true,
-  },
-  filename: "src/styles/complete_configless.css",
-  // minify: true,
+const { code: codeComplete } = bundle({
+    // ...
+    targets,
+    drafts: {
+        customMedia: true,
+    },
+    filename: 'src/styles/complete.css',
+    // minify: true,
 });
 
-fs.writeFileSync("out/complete_configless.css", codeCompleteConfigless);
+fs.writeFileSync('out/complete.css', codeComplete);
+
+const { code: codeCompleteConfigless } = bundle({
+    // ...
+    targets,
+    drafts: {
+        customMedia: true,
+    },
+    filename: 'src/styles/complete_configless.css',
+    // minify: true,
+});
+
+fs.writeFileSync('out/complete_configless.css', codeCompleteConfigless);
