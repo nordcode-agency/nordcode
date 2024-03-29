fdf826f52c1087658c65d97f953d72d1f2acf890
Lukas Koeller <acc.koeller@protonmail.com>
2024-03-29 17:49:00 +0100

    chore: work on cta

--- apps/web/src/lib/modules/common/components/Cta.svelte
+++ apps/web/src/lib/modules/common/components/Cta.svelte
@@ -1,27 +1,77 @@
-<section class="nc-box">
-    <div class="box nc-stack -farthest">
-        <h2>
-            {#each [...'norcode'] as char, idx}
-                <div class="char-animation" style={`--char-animation-delay: ${idx * 120}ms`}>{char}</div>
-            {/each}
-        </h2>
+<section>
+    <div class="oval-wrapper">
+        <div class="oval"></div>
+    </div>
+    <div class="nc-box box">
         <button type="button" class="nc-button">Loslegen</button>
     </div>
 </section>
 
 <style lang="postcss">
     section {
+        --shadow-blur: 32px;
+        --shadow-vertical-offset: -8px;
+        --shadow-max-multiplier: 4;
+
         inline-size: 100%;
-        block-size: 70dvh;
+        min-block-size: 80vh;
         display: grid;
         place-items: center;
-    }
-
-    h2 {
-        display: flex;
+        padding-inline: 0;
+        grid: "cta" 1fr / 1fr
     }
 
     .box {
-        align-items: center;
+        grid-area: cta;
+        margin-block-end: calc(var(--shadow-blur) * var(--shadow-max-multiplier));
+    }
+
+    .oval-wrapper {
+        grid-area: cta;
+        padding-block: calc(
+            60px +
+            var(--shadow-blur) *
+            var(--shadow-max-multiplier) +
+            var(--shadow-vertical-offset) * 
+            -1
+        );
+        min-inline-size: 0;
+        inline-size: 100%;
+        block-size: calc(var(--shadow-blur) * 10);
+        overflow: hidden;
+        display: flex;
+        justify-content: center;
+    }
+
+    .oval {
+        inline-size: 300vw;
+        block-size: 300vw;
+        margin-inline: auto;
+        border-radius: 100%;
+        flex-shrink: 0;
+        inset: 0;
+        animation-duration: 1ms;
+        animation: appear linear both;
+        animation-timeline: --atmosphereTimeline;
+        animation-range: 86% 96%;
+        box-shadow:
+            0px var(--shadow-vertical-offset) calc(var(--shadow-blur) * var(--shadow-max-multiplier) / 8) 0px rgba(255,255,255,0.7),
+            0px var(--shadow-vertical-offset) calc(var(--shadow-blur) * var(--shadow-max-multiplier) / 4) 0px rgba(255,255,255,0.5),
+            0px var(--shadow-vertical-offset) calc(var(--shadow-blur) * var(--shadow-max-multiplier) / 2) 0px var(--color-brand-secondary-dark-emphasis),
+            0px var(--shadow-vertical-offset) calc(var(--shadow-blur) * var(--shadow-max-multiplier)) 0px var(--color-brand-primary-light-emphasis);
+    }
+
+    @keyframes appear {
+        0% {
+            opacity: 0;
+            transform: scaleX(0.5);
+        }
+        6% {
+            transform: scaleX(0.5);
+        }
+        100% {
+            opacity: 1;
+            transform: scaleX(1)
+        }
     }
 </style>
--- apps/web/src/lib/modules/common/components/Footer.svelte
+++ apps/web/src/lib/modules/common/components/Footer.svelte
@@ -1,3 +1,9 @@
 <footer class="nc-box">
   Liebe Grüße aus Zeetze & Hamburg
 </footer>
+
+<style lang="postcss">
+    footer {
+        min-inline-size: 40vh;
+    }
+</style>
