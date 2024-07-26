97ac85b3398f20c985b665508c7e0ac3b73b63a1
Lukas Koeller <acc.koeller@protonmail.com>
2024-07-26 20:37:38 +0200

    feat: add SecondaryCtaButton

---
+++ apps/web/src/lib/modules/common/components/SecondaryCtaButton.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	export let href: string;
+</script>
+
+<a {href} class="nc-button -stealth gradient-text">
+	<slot />
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		class="nc-icon"
+		data-size="inline"
+		stroke="var(--color-brand-secondary-base)"
+		viewBox="0 0 24 24"
+		stroke-width="2"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M5 12l14 0" />
+		<path d="M15 16l4 -4" />
+		<path d="M15 8l4 4" />
+	</svg>
+</a>
+
+<style lang="postcss">
+	a {
+		padding-inline-start: 0;
+
+		& svg {
+			transition: transform var(--transition-duration-base) var(--ease-elastic-2);
+		}
+
+		&:hover svg {
+			transform: translateX(0.5ch);
+		}
+	}
+</style>
