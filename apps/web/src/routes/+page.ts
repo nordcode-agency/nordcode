6c464b992e17d0a3e0f78c4a4a619f9eba097e40
Lukas Koeller <acc.koeller@protonmail.com>
2024-08-28 23:02:02 +0200

    chore(web): add and edit content to head

--- apps/web/src/routes/+page.ts
+++ apps/web/src/routes/+page.ts
@@ -2,10 +2,10 @@ import type { PageLoad } from './$types';
 import { projects } from '$lib/content/index';
 
 export const load = (async () => {
-
 	return {
-		title: 'nordcode – we schaffen ein neues digitales Erlebnis.',
-		content: 'Hallo, Freunde.',
-		projects,
+		title: 'nordcode – wir schaffen ein neues digitales Erlebnis.',
+		content:
+			'Wir sind eine Webagentur mit Leidenschaft für schönes Design und User Experience. Wir entwickeln Websites, Webapps, E-Commerce Lösungen z.B mit Shopify, native Apps für iOS und Android. Wir unterstützen bei React oder Vue sowie CMS wie WordPress.',
+		projects
 	};
 }) satisfies PageLoad;
