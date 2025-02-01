f5add6d19e15346c60d16af83f687811441c960a
Joshua Stübner <joshua.stuebner@orgavision.com>
2025-02-01 13:50:02 +0100

    feat: add stahlmann case

---
+++ +page.ts
@@ -0,0 +1,13 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+
+export const load = (async () => {
+	const { projects } = await import('$lib/content');
+	const project = projects.find((prj) => prj.name === 'wildtierrettung-rhein-berg');
+
+	if (!project) {
+		error(404, `Projekt "Wildtierrettung" konnte nicht gefunden werden.`);
+	}
+
+	return { ...project };
+}) satisfies PageLoad;
