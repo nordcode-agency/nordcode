39940928ab0742ac5b514250ad060b7df87fc4da
Joshua Stübner <joshua.stuebner@orgavision.com>
2025-02-01 13:49:18 +0100

    feat: refactor blogImge into component

---
+++ apps/web/src/lib/modules/common/components/BlogImagePreview.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+import type { SvelteHTMLElements } from "svelte/elements";
+
+interface Props {
+    image: SvelteHTMLElements['enhanced:img'];
+    children: Snippet;
+}
+
+let {image, children}: Props = $props();
+
+</script>
+
+<figure class="case-img-container card-bg">
+    <a href={image.img.src} aria-label="Bild vergrößern">
+    <enhanced:img class="case-img" src={image} />
+    </a>
+	<figcaption>
+	   {@render children()}
+	</figcaption>
+</figure>
