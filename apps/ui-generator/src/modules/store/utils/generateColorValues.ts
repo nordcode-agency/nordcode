10bfaa5de258b47d94884c482c994e8173f1849f
Joshua Stübner <joshua.stuebner@orgavision.com>
2026-02-23 10:05:24 +0100

    feat(ui-gen): fix contrast color logic

--- apps/ui-generator/src/modules/store/utils/generateColorValues.ts
+++ apps/ui-generator/src/modules/store/utils/generateColorValues.ts
@@ -18,7 +18,7 @@ const generateLightColorValues = (
     const cTokenName = `--c-${tokenName}`;
     const hTokenName = `--h-${tokenName}`;
     const baseColorName = `--color-${tokenName}-base-${themeSuffix}`;
-    const contrastToken = colorDef.l < contrastCutOff ? 'var(--lightness-min)' : 'var(--lightness-max)';
+    const contrastToken = colorDef.l > contrastCutOff ? 'var(--lightness-min)' : 'var(--lightness-max)';
 
     return {
         [lTokenName]: `${colorDef.l / 100}`,
@@ -43,7 +43,7 @@ const generateDarkColorValues = (
     const cTokenName = `--c-${tokenName}`;
     const hTokenName = `--h-${tokenName}`;
     const baseColorName = `--color-${tokenName}-base-${themeSuffix}`;
-    const contrastToken = colorDef.l < contrastCutOff ? 'var(--lightness-min)' : 'var(--lightness-max)';
+    const contrastToken = colorDef.l > contrastCutOff ? 'var(--lightness-min)' : 'var(--lightness-max)';
 
     return {
         [lTokenName]: `${colorDef.lDark / 100}`,
