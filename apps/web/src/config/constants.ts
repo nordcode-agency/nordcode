a145e18f7adce35e1f5ca5c6a5871fb089cb8af2
Lukas Koeller <acc.koeller@protonmail.com>
2026-05-25 12:16:21 +0200

    fix: use updated whatsapp link

--- apps/web/src/config/constants.ts
+++ apps/web/src/config/constants.ts
@@ -1,6 +1,6 @@
 export const PHONE_NUMBER = "+1234567890";
 export const EMAIL_ADDRESS = "moin@nordcode.agency";
-export const WA_LINK = "https://wa.me/message/6D6KQFSIQQ6EG1";
+export const WA_LINK = "https://wa.me/4915563134234";
 
 export const EmailSubjects = {
     informationAboutObject: "Informationen zu Objekt",
